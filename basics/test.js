9ed5560f80768825f8006bcc2fbc44440106e7c5
Shaswati Chakraborty <97435009+Shaswati101@users.noreply.github.com>
2025-10-28 16:14:26 +0000

    Lesson 5 added

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,38 @@
+// Lesson 3
 console.log("Kyoshi")
+
+// Lesson 4
+const name = "Kyoshi"
+/* Scope of const is that you cannot reassign or chagne the value of the variable.
+   */
+
+let id = 1234
+var roll = 1495
+sub = "JavaScript"
+
+// Lesson 5
+"Use Strict"; // This command treats all further JS code as per it's newer version
+
+// PRIMITIVE Data Types in JavaScript
+// number - can support any number upto 2^ 53
+// bigint - can support numbers bigger than number data type
+// string - always prefer "" to wrap your string
+// boolean - true/false
+// null - a standalone value in JS which acts as a place holder for empty value
+   // eg: say, a server was to send us the temperature of a place upon request.
+   // But due to some technical issues it couldnt send any data.
+   // In this case, we can't assume the value to be 0 because that is also a valid temperature.
+   // Thus comes "null" to save the day and take the place of the empty/no value.
+
+// undefined - the value of a variable that has been declared but no value for that variable has been set
+// symbol - a lil unique but we'll come back to it while learning about React
+// 
+
+
+// ADVANCED Data Type
+// object
+
+// console.log(typeof undefined); // tyepof keyword lets us know the data type of the value passed after it. In this case it's undefined
+// console.log(typeof null) // object data type
+
+

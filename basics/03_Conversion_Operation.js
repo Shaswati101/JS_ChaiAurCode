59d25a5da9e0689a53594df996a2cfae193efc1c
Shaswati Chakraborty <97435009+Shaswati101@users.noreply.github.com>
2025-10-30 03:25:25 +0000

    Lesson 6 on conversion and operations

---
+++ basics/03_Conversion_Operation.js
@@ -0,0 +1,6 @@
+let value = 33
+
+console.log(typeof value) // Number
+
+let value_in_num = "33" // To us, it might look like a number. But it is a string
+console.log(typeof value_in_num); // String

fe9f621612519f26df30186cf0e5f4b6fb91c49f
Shaswati Chakraborty <97435009+Shaswati101@users.noreply.github.com>
2025-10-30 07:51:29 +0000

    Rectified some grammatical errors

--- basics/03_Conversion_Operation.js
+++ basics/03_Conversion_Operation.js
@@ -1,6 +1,28 @@
-let value = 33
+// Lesson 6
+let score = 33
+console.log(typeof score); // number
 
-console.log(typeof value) // Number
+let score_in_str = "33"
+console.log(typeof score_in_str); // string
 
-let value_in_num = "33" // To us, it might look like a number. But it is a string
-console.log(typeof value_in_num); // String
+
+// What if we wanted to convert score_in_str into a number?
+let score_in_num = Number(score_in_str)
+console.log(typeof score_in_num); // number
+
+
+// What if we convert an alphanumeric?
+let score_aphanum = "33abc"
+let score_only_num = Number(score_aphanum)
+console.log(typeof score_only_num); // number
+
+// But... There's a twist!
+console.log(score_only_num);
+/*
+NaN(Not a Number)! But Why?
+That's because any alphanumeric character cannot be converted into an actual number.
+But since JS is a loosely typed language, it will not directly throw an error.
+However, it converts alphanums into NaN
+*/
+
+

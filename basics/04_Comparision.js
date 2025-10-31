84aa971b31881a5ab70c7323a018c3dd67bc70a7
Shaswati Chakraborty <97435009+Shaswati101@users.noreply.github.com>
2025-10-31 07:09:05 +0000

    Created a new file 04_Conversion

---
+++ basics/04_Comparision.js
@@ -0,0 +1,10 @@
+// Here we will learn about comparision between same and different data types
+
+// Some basic form of comparison would be:
+console.log(2 > 2);
+console.log(2 < 2);
+console.log(2 == 2);
+// etc. You know the drill
+
+//But, what if we try to convert a string with a number?
+console.log("a"==2);
